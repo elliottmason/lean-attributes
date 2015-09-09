6f16208eb672a37d3be051831b3a1a0a6d04069e
R. Elliott Mason <eleo@fastmail.fm>
2015-09-09 16:57:02 -0400

    Adds as Basic module with limited functionality

--- lib/lean-attributes/attributes.rb
+++ lib/lean-attributes/attributes.rb
@@ -1,13 +1,22 @@
 require 'lean-attributes/attributes/class_methods'
-require 'lean-attributes/attributes/coercion'
+require 'lean-attributes/attributes/coercion_helpers'
 require 'lean-attributes/attributes/initializer'
 
 module Lean
   module Attributes
     def self.included(base)
       base.class_eval do
-        extend ClassMethods
-        include Coercion
+        include Basic
+        include CoercionHelpers
+        include Initializer
+      end
+    end
+
+    module Basic
+      def self.included(base)
+        base.class_eval do
+          extend ClassMethods
+        end
       end
     end
   end
