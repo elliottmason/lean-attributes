e1125a3d54b4e7d9b5aa511fa450c03a4e176d1b
R. Elliott Mason <eleo@fastmail.fm>
2015-09-18 13:56:28 -0400

    Slightly improves benchmark formatting

--- benchmarks/comparison.rb
+++ benchmarks/comparison.rb
@@ -52,21 +52,21 @@ Benchmark.ips do |x|
   strings  = { attr0: '0', attr1: '1', attr2: '2', attr3: '3', attr4: '4',
                attr5: '5', attr6: '6', attr7: '7', attr8: '8', attr9: '9' }
 
-  x.report('Lean::Attributes: without values')                        { LeanIntegers.new }
-  x.report('Lean::Attributes: integer values for integer attributes') { LeanIntegers.new(integers) }
-  x.report('Lean::Attributes: string values for integer attributes ') { LeanIntegers.new(strings) }
+  x.report('Lean::Attributes: without values                        ') { LeanIntegers.new }
+  x.report('Lean::Attributes: integer values for integer attributes ') { LeanIntegers.new(integers) }
+  x.report('Lean::Attributes: string values for integer attributes  ') { LeanIntegers.new(strings) }
 
-  x.report('FastAttributes: without values                       ') { FastIntegers.new }
-  x.report('FastAttributes: integer values for integer attributes') { FastIntegers.new(integers) }
-  x.report('FastAttributes: string values for integer attributes ') { FastIntegers.new(strings) }
+  x.report('FastAttributes: without values                          ') { FastIntegers.new }
+  x.report('FastAttributes: integer values for integer attributes   ') { FastIntegers.new(integers) }
+  x.report('FastAttributes: string values for integer attributes    ') { FastIntegers.new(strings) }
 
-  x.report('Attrio: without values                               ') { AttrioIntegers.new }
-  x.report('Attrio: integer values for integer attributes        ') { AttrioIntegers.new(integers) }
-  x.report('Attrio: string values for integer attributes         ') { AttrioIntegers.new(strings) }
+  x.report('Attrio: without values                                  ') { AttrioIntegers.new }
+  x.report('Attrio: integer values for integer attributes           ') { AttrioIntegers.new(integers) }
+  x.report('Attrio: string values for integer attributes            ') { AttrioIntegers.new(strings) }
 
-  x.report('Virtus: without values                               ') { VirtusIntegers.new }
-  x.report('Virtus: integer values for integer attributes        ') { VirtusIntegers.new(integers) }
-  x.report('Virtus: string values for integer attributes         ') { VirtusIntegers.new(strings) }
+  x.report('Virtus: without values                                  ') { VirtusIntegers.new }
+  x.report('Virtus: integer values for integer attributes           ') { VirtusIntegers.new(integers) }
+  x.report('Virtus: string values for integer attributes            ') { VirtusIntegers.new(strings) }
 
   x.compare!
 end
